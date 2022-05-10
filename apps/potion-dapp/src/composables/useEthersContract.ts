fe1c42ce1d8a19d29c9acb1bcaa551988344cb8f
Raz <me@raz.works>
2022-05-10 08:47:04 +0200

    feat: Scaffolded potion-dapp package

---
+++ apps/potion-dapp/src/composables/useEthersContract.ts
@@ -0,0 +1,44 @@
+import { useEthersProvider } from "./useEthersProvider";
+import { useEthersSigner } from "./useEthersSigner";
+
+export function useEthersContract() {
+  const { initProvider } = useEthersProvider();
+  const { getSigner } = useEthersSigner();
+  const initContract = (
+    // signerOrProvider: JsonRpcSigner | JsonRpcProvider,
+    needSigner = false,
+    webSocket = false,
+    typechainContractFactory: any,
+    contractAddress: string
+  ) => {
+    try {
+      if (webSocket && needSigner) {
+        throw new Error(
+          "cannot initialize the contract with a webSocket and a signer"
+        );
+      }
+      if (webSocket) {
+        return typechainContractFactory.connect(
+          contractAddress,
+          initProvider(true)
+        );
+      }
+      if (needSigner) {
+        return typechainContractFactory.connect(contractAddress, getSigner());
+      }
+      return typechainContractFactory.connect(
+        contractAddress,
+        initProvider(false)
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      } else {
+        throw new Error("cannot initialize the ethers contract");
+      }
+    }
+  };
+  return {
+    initContract,
+  };
+}
---
+++ apps/potion-dapp/src/views/AboutView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-white">About Page</h1>
+</template>
