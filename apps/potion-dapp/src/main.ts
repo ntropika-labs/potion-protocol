78123686c10c48fb0e7b52ecbe0a78179ad3a20b
bained15 <alessio.buscaini92@gmail.com>
2022-05-16 12:48:11 +0200

    feat(potion-dapp): setup and configure i18n

--- apps/potion-dapp/src/main.ts
+++ apps/potion-dapp/src/main.ts
@@ -3,6 +3,8 @@ import "@unocss/reset/tailwind.css";
 
 import { createPinia } from "pinia";
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import messages from "@intlify/vite-plugin-vue-i18n/messages";
 
 import App from "@/App.vue";
 import { init } from "@/composables/useOnboard";
@@ -11,6 +13,13 @@ import router from "@/router";
 import urql from "@urql/vue";
 
 init(onboardOptions);
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "en",
+  messages,
+});
 const app = createApp(App);
 
 app.use(urql, {
@@ -20,5 +29,6 @@ app.use(urql, {
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
