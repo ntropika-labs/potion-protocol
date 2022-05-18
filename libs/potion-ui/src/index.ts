a4e0ebe2e1504113b72dfb45be5bdecfd034efaf
Raz <me@raz.works>
2022-05-18 07:41:04 +0200

    exported components

--- libs/potion-ui/src/index.ts
+++ libs/potion-ui/src/index.ts
@@ -1,5 +1,9 @@
 import BaseButton from "./components/BaseButton/BaseButton.vue";
+import BaseCard from "./components/BaseCard/BaseCard.vue";
+import BaseTag from "./components/BaseTag/BaseTag.vue";
+import CardFooter from "./components/CardFooter/CardFooter.vue";
 import ConnectWalletButton from "./components/ConnectWalletButton/ConnectWalletButton.vue";
+import InputNumber from "./components/InputNumber/InputNumber.vue";
 import HeaderComponent from "./components/layout/Header/HeaderComponent.vue";
 
 export type {
@@ -15,4 +19,12 @@ export type {
  * Here, we export each of the components as a named export.
  * TODO: Add a folder per component and export the SFC as a named export in a TS file so we can globally import them.
  */
-export { BaseButton, HeaderComponent, ConnectWalletButton };
+export {
+  BaseButton,
+  HeaderComponent,
+  ConnectWalletButton,
+  BaseCard,
+  BaseTag,
+  CardFooter,
+  InputNumber,
+};
