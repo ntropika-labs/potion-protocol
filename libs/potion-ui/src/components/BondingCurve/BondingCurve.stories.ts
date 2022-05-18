c82eb0a638ad4322dc49b520947b628297bbc321
Alberto <albertopastrolin@gmail.com>
2022-05-18 13:12:00 +0200

    chore: improved story

--- libs/potion-ui/src/components/BondingCurve/BondingCurve.stories.ts
+++ libs/potion-ui/src/components/BondingCurve/BondingCurve.stories.ts
@@ -1,10 +1,12 @@
 // @unocss-include
 
 import { HyperbolicCurve } from "contracts-math";
-import { times as _times } from "lodash-es";
+import { getTokenList } from "potion-tokenlist";
+import { times as _times, random as _random } from "lodash-es";
 import BondingCurve from "./BondingCurve.vue";
 import type { Args, Story } from "@storybook/vue3";
 
+const tokenList = getTokenList("ganache");
 const curvePoints = 100;
 const getCurvePoints = (curve: HyperbolicCurve) =>
   _times(curvePoints, (x: number) => curve.evalAt(x / curvePoints));
@@ -14,17 +16,26 @@ export default {
   excludeStories: /.*Data$/,
   title: "Potion UI/Charts/Bonding Curve",
   argTypes: {
-    aParam: { control: "number" },
-    bParam: { control: "number" },
-    cParam: { control: "number" },
-    dParam: { control: "number" },
-    maxUtilization: { control: "number" },
+    aParam: { control: { type: "number", min: 0, max: 10, step: 0.01 } },
+    bParam: { control: { type: "number", min: 0, max: 10, step: 0.01 } },
+    cParam: { control: { type: "number", min: 0, max: 10, step: 0.01 } },
+    dParam: { control: { type: "number", min: 0, max: 10, step: 0.01 } },
+    maxUtilization: { control: { type: "number", min: 0, max: 1, step: 0.01 } },
   },
 };
 
 const Template: Story<Args> = (args: Args) => ({
   components: { BondingCurve },
   setup() {
+    const getRandomCurve = () =>
+      new HyperbolicCurve(
+        _random(args.aParam * 0.5, args.aParam * 1.5, true),
+        _random(args.bParam * 0.5, args.bParam * 1.5, true),
+        _random(args.cParam * 0.5, args.cParam * 1.5, true),
+        _random(args.dParam * 0.5, args.dParam * 1.5, true),
+        _random(0, 1, true)
+      );
+
     return {
       bondingCurve: getCurvePoints(
         new HyperbolicCurve(
@@ -35,24 +46,10 @@ const Template: Story<Args> = (args: Args) => ({
           args.maxUtilization
         )
       ).map((p: number) => p * 100),
-      emergingCurves: [
-        {
-          data: getCurvePoints(new HyperbolicCurve(1.5, 2.5, 2.5, 2.5, 1)),
-          underlyingSymbol: "WETH",
-        },
-        {
-          data: getCurvePoints(new HyperbolicCurve(2.5, 1.5, 2.5, 2.5, 1)),
-          underlyingSymbol: "UNI",
-        },
-        {
-          data: getCurvePoints(new HyperbolicCurve(2.5, 2.5, 1.5, 2.5, 1)),
-          underlyingSymbol: "AAVE",
-        },
-        {
-          data: getCurvePoints(new HyperbolicCurve(2.5, 2.5, 2.5, 1.5, 1)),
-          underlyingSymbol: "WBTC",
-        },
-      ],
+      emergingCurves: tokenList.map(({ symbol }) => ({
+        data: getCurvePoints(getRandomCurve()),
+        underlyingSymbol: symbol,
+      })),
     };
   },
   template: `<div class="max-w-[64rem]">
