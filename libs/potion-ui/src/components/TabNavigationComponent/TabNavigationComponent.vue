13af8e7d7c271a6f0db90c282359c6b20d18f6ab
bained15 <alessio.buscaini92@gmail.com>
2022-05-19 10:36:41 +0200

    wip(potion-ui): rework tab navigation component logic

--- libs/potion-ui/src/components/TabNavigationComponent/TabNavigationComponent.vue
+++ libs/potion-ui/src/components/TabNavigationComponent/TabNavigationComponent.vue
@@ -1,13 +1,11 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref, useSlots, watch } from "vue";
-import { BaseButton, TabComponent } from "../../index";
+import { defineComponent, ref, watch, type ComputedRef } from "vue";
+import { BaseButton } from "../../index";
 
 interface TabNavigationInfo {
   label: string;
-  visited: boolean;
-  isValid: boolean;
-  component: typeof TabComponent;
-  //htmlElement: Element;
+  visited?: boolean;
+  isValid: ComputedRef<boolean>;
 }
 
 export default defineComponent({
@@ -17,6 +15,7 @@ export default defineComponent({
 <script lang="ts" setup>
 import BaseCard from "../BaseCard/BaseCard.vue";
 export interface Props {
+  tabs: Array<TabNavigationInfo>;
   defaultIndex?: number;
   vertical?: boolean;
   hasNavigation?: boolean;
@@ -33,32 +32,10 @@ const emit = defineEmits<{
 }>();
 
 const currentIndex = ref<number>(props.defaultIndex);
-const tabItems = ref<Array<TabNavigationInfo>>([]);
-
-const internalSlots = useSlots();
-const setupTabs = () => {
-  console.log(internalSlots);
-  const navItems: Array<TabNavigationInfo> = [];
-  if (internalSlots && internalSlots.default) {
-    const slots = internalSlots.default() || [];
-
-    slots.forEach((component: any, i: number) => {
-      if (component.type.name === "TabComponent") {
-        navItems.push({
-          label: component?.props.title,
-          component: component,
-          visited: false,
-          isValid: component?.props.isValid,
-        });
-      }
-    });
-
-    tabItems.value = navItems;
-  }
-};
 
 const setCurrentIndex = (index: number) => {
-  currentIndex.value = Math.max(Math.min(index, tabItems.value.length - 1), 0);
+  //currentIndex.value = Math.max(Math.min(index, tabItems.value.length - 1), 0);
+  currentIndex.value = Math.max(index, 0);
 };
 
 const navigateToTab = (index: number) => {
@@ -69,35 +46,11 @@ const navigateToTab = (index: number) => {
   }
 };
 
-const updateTabNavigationItems = () => {
-  if (currentIndex.value > -1 && currentIndex.value < tabItems.value.length) {
-    console.log(
-      "navigating to tab",
-      "current index: ",
-      currentIndex.value,
-      props.defaultIndex
-    );
-
-    tabItems.value.forEach((tab, index) => {
-      tab.visited = currentIndex.value >= index;
-      tab.isValid =
-        tab.component.props.isValid || tab.component.props["is-valid"];
-
-      console.log(JSON.stringify(tab.component));
-    });
-  }
-};
-
-onMounted(() => {
-  setupTabs();
-  console.log(tabItems.value, props.defaultIndex, currentIndex.value);
-});
-
 watch(
   () => props.defaultIndex,
   (value) => {
     setCurrentIndex(value);
-    updateTabNavigationItems();
+    //updateTabNavigationItems();
   },
   { immediate: true }
 );
@@ -107,7 +60,7 @@ watch(
   <BaseCard>
     <ul class="inline-flex items-center justify-evenly gap-4 mx-auto pt-2 pb-6">
       <li
-        v-for="(step, index) in tabItems"
+        v-for="(step, index) in tabs"
         :key="index"
         class="text-center relative before:(content-none flex absolute bottom-0 left-[50%] w-full h-[2px] translate-x-[-50%])"
         :class="[
@@ -132,36 +85,12 @@ watch(
   </BaseCard>
   <!-- End tab navigation -->
   <!-- Start tabs content -->
-  <div class="overflow-hidden mt-6">
-    <template v-for="(step, index) in tabItems">
-      <component
-        :is="step.component"
-        v-if="index === currentIndex"
-        :key="index"
-      ></component>
+  <div v-if="$slots.default" class="overflow-hidden mt-6">
+    <template v-for="(step, index) in $slots.default()" :key="index">
+      <KeepAlive>
+        <component :is="step" v-if="index === currentIndex"></component>
+      </KeepAlive>
     </template>
   </div>
   <!-- End tabs content -->
-  <!-- Start button navigation -->
-  <div v-if="props.hasNavigation" class="flex justify-between p-4">
-    <BaseButton
-      :label="currentIndex === 0 ? 'Cancel' : 'Back'"
-      palette="transparent"
-      @click="navigateToTab(currentIndex - 1)"
-    >
-      <template v-if="currentIndex > 0" #pre-icon>
-        <i class="i-ph-caret-left"></i>
-      </template>
-    </BaseButton>
-    <BaseButton
-      :label="currentIndex < tabItems.length - 1 ? 'Next' : 'Complete'"
-      palette="secondary"
-      @click="navigateToTab(currentIndex + 1)"
-    >
-      <template #post-icon>
-        <i class="i-ph-caret-right"></i>
-      </template>
-    </BaseButton>
-  </div>
-  <!-- End button navigation -->
 </template>
