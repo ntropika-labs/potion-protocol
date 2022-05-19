b9239f81b8811ab66292b6d90aac0a41585ba7c8
bained15 <alessio.buscaini92@gmail.com>
2022-05-19 15:29:40 +0200

    fix(potion-dapp): remove global layout registration

--- apps/potion-dapp/src/main.ts
+++ apps/potion-dapp/src/main.ts
@@ -8,8 +8,6 @@ import { createI18n } from "vue-i18n";
 import App from "@/App.vue";
 import { init } from "@/composables/useOnboard";
 import { onboardOptions } from "@/helpers/onboard";
-import BaseLayout from "@/layouts/BaseLayout.vue";
-import EmptyLayout from "@/layouts/EmptyLayout.vue";
 import router from "@/router";
 import messages from "@intlify/vite-plugin-vue-i18n/messages";
 import urql from "@urql/vue";
@@ -32,7 +30,5 @@ app.use(urql, {
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
-app.component("BaseLayout", BaseLayout);
-app.component("EmptyLayout", EmptyLayout);
 
 app.mount("#app");
