7f12723aa919e3ad9dafc3468eea46b98f210745
Raz <me@raz.works>
2022-05-20 14:22:31 +0200

    export helpers from potion-ui

--- libs/potion-ui/src/index.ts
+++ libs/potion-ui/src/index.ts
@@ -1,15 +1,18 @@
+//helpers
+export * from "./helpers";
+
 import BaseButton from "./components/BaseButton/BaseButton.vue";
 import BaseCard from "./components/BaseCard/BaseCard.vue";
 import BaseTag from "./components/BaseTag/BaseTag.vue";
+import BondingCurve from "./components/BondingCurve/BondingCurve.vue";
 import CardFooter from "./components/CardFooter/CardFooter.vue";
 import ConnectWalletButton from "./components/ConnectWalletButton/ConnectWalletButton.vue";
+import CurveFormula from "./components/CurveFormula/CurveFormula.vue";
+import CustomCurveParams from "./components/CustomCurveParams/CustomCurveParams.vue";
 import InputNumber from "./components/InputNumber/InputNumber.vue";
 import HeaderComponent from "./components/layout/Header/HeaderComponent.vue";
 import TabNavigationComponent from "./components/TabNavigationComponent/TabNavigationComponent.vue";
 import UnderlyingSelection from "./components/UnderlyingSelection/UnderlyingSelection.vue";
-import CustomCurveParams from "./components/CustomCurveParams/CustomCurveParams.vue";
-import CurveFormula from "./components/CurveFormula/CurveFormula.vue";
-import BondingCurve from "./components/BondingCurve/BondingCurve.vue";
 
 export type {
   IconStrokeWeight,
