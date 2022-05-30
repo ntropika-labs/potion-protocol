0f4bf35f1d7212a9efcf4786f2882b3ac5c48ba8
Alberto <albertopastrolin@gmail.com>
2022-05-30 18:25:07 +0200

    feat: ported chart from the old application

---
+++ libs/potion-ui/src/components/PerformanceChart/PerformanceChart.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+
+import PerformanceChart from "./PerformanceChart.vue";
+
+describe("PerformanceChart", () => {
+  it("compiles properly", () => {
+    expect(PerformanceChart).toBeTruthy();
+  });
+});
