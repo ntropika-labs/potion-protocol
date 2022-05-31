c57eafd9801df73b723ef2a10b1718955ef98e59
Alberto <albertopastrolin@gmail.com>
2022-05-31 18:50:56 +0200

    chore: use unref to support both reactive values and non-reactive ones

--- apps/potion-dapp/src/composables/useEmergingCurves.ts
+++ apps/potion-dapp/src/composables/useEmergingCurves.ts
@@ -1,17 +1,19 @@
 import { getPoolsFromCriterias } from "potion-router";
 import { worker } from "@/web-worker";
-import { ref } from "vue";
+import { ref, unref } from "vue";
 
 import type { Ref, ComputedRef } from "vue";
 import type { EmergingCurvePoints, Criteria } from "dapp-types";
 
 export const useEmergingCurves = (
-  criterias: Ref<Criteria[]> | ComputedRef<Criteria[]>
+  criterias: Ref<Criteria[]> | ComputedRef<Criteria[]> | Criteria[]
 ) => {
   const emergingCurves = ref<EmergingCurvePoints[]>([]);
 
   const loadEmergingCurves = async () => {
-    const poolSets = await getPoolsFromCriterias(criterias.value);
+    // this returns us ComputedRef<Criteria[]> | Criteria[] so we need to unref it again
+    const unreffedCriterias = unref(criterias);
+    const poolSets = await getPoolsFromCriterias(unref(unreffedCriterias));
     emergingCurves.value = await worker.getEmergingBondingCurvesFromCriterias(
       poolSets
     );
