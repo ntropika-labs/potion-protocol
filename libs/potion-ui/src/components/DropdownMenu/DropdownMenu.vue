f311d2b669c0379960006ba28f5b3ca93ab856e4
Alberto <albertopastrolin@gmail.com>
2022-06-01 17:05:02 +0200

    feat: handle outside clicks + some style fixes

--- libs/potion-ui/src/components/DropdownMenu/DropdownMenu.vue
+++ libs/potion-ui/src/components/DropdownMenu/DropdownMenu.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { onClickOutside } from "@vueuse/core";
 import { ref } from "vue";
 import type { MenuItem } from "dapp-types";
 
@@ -10,6 +11,7 @@ export interface Props {
 
 const props = defineProps<Props>();
 const open = ref(false);
+const dropdownMenuRef = ref<HTMLDivElement>();
 
 const emits = defineEmits<{
   (e: "item-selected", name: string): void;
@@ -17,17 +19,19 @@ const emits = defineEmits<{
 
 const toggle = () => (open.value = !open.value);
 const hasStatus = (name: string) => props.itemsSelectedStatus.has(name);
+
+onClickOutside(dropdownMenuRef, () => (open.value = false));
 </script>
 
 <template>
-  <div class="relative inline-block text-white">
+  <div ref="dropdownMenuRef" class="relative inline-block text-white">
     <div
-      class="inline-flex items-center px-4 p-1.5 bg-deep-black-700 rounded-full cursor-pointer hover:bg-gradient-to-r hover:from-primary-500 hover:to-primary-400"
+      class="inline-flex items-center px-4 p-1.5 bg-deep-black-700 rounded-full cursor-pointer hover:(bg-gradient-to-r from-primary-500 to-primary-400)"
       @click="toggle"
     >
       <div class="pr-3 text-xs">{{ title }}</div>
       <span
-        class="h-3 i-ph-caret-up"
+        class="h-3"
         :class="open ? 'i-ph-caret-up' : 'i-ph-caret-down'"
       ></span>
     </div>
@@ -38,9 +42,9 @@ const hasStatus = (name: string) => props.itemsSelectedStatus.has(name);
       <span
         v-for="item in items"
         :key="item.name"
-        class="modal-item flex text-xs py-1 px-4 cursor-pointer hover:bg-primary-500 bg-opacity-10"
+        class="modal-item flex text-xs py-1 px-4 cursor-pointer hover:bg-primary-500"
         :class="{
-          'text-opacity-30':
+          'text-white/30':
             hasStatus(item.name) && !itemsSelectedStatus.get(item.name),
         }"
         @click="emits('item-selected', item.name)"
