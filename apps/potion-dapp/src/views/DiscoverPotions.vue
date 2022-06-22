6f52bcfb290d88ba581ecdc4f450f5b43c2fa59d
Raz <me@raz.works>
2022-06-22 07:25:40 +0200

    initial most purchased and most collateralized panels

--- apps/potion-dapp/src/views/DiscoverPotions.vue
+++ apps/potion-dapp/src/views/DiscoverPotions.vue
@@ -8,20 +8,81 @@
   >
   </JumboHeader>
   <InnerNav v-bind="innerNavProps" class="mt-10" />
+  <BaseCard class="p-4 mt-10">
+    <h1 class="uppercase text-secondary-500 text-xs">
+      {{ t("most_purchased") }}
+    </h1>
+    <p class="mt-3">{{ t("most_purchased_description") }}</p>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-6"
+    >
+      <PotionCard
+        v-for="ptn in loadedMostPurchasedPotions"
+        :key="ptn.id"
+        :token="
+          underlyingToToken(
+            ptn.underlyingAsset.address,
+            parseInt(ptn.underlyingAsset.decimals)
+          )
+        "
+        :otoken-address="ptn.tokenAddress"
+        :strike-price="ptn.strikePrice"
+        :expiration="ptn.expiry"
+      />
+    </div>
+  </BaseCard>
+  <BaseCard class="p-4 mt-10">
+    <h1 class="uppercase text-secondary-500 text-xs">
+      {{ t("most_collateralized") }}
+    </h1>
+    <p class="mt-3">{{ t("most_collateralized_description") }}</p>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-6"
+    >
+      <PotionCard
+        v-for="ptn in loadedMostPurchasedPotions"
+        :key="ptn.id"
+        :token="
+          underlyingToToken(
+            ptn.underlyingAsset.address,
+            parseInt(ptn.underlyingAsset.decimals)
+          )
+        "
+        :otoken-address="ptn.tokenAddress"
+        :strike-price="ptn.strikePrice"
+        :expiration="ptn.expiry"
+      />
+    </div>
+  </BaseCard>
+
+  <pre>{{ underlyingsWithLiquidity }}</pre>
+  <pre>{{ loadedMostPurchasedPotions }}</pre>
+  <pre>{{ loadedMostCollateralizedPotions }}</pre>
 </template>
 <script lang="ts" setup>
 import InnerNav from "@/components/InnerNav.vue";
+import type { Token } from "dapp-types";
+import { PotionCard, BaseCard } from "potion-ui";
+import {
+  useGetMostPurchasedPotionsQuery,
+  useGetMostCollateralizedPotionsQuery,
+} from "subgraph-queries/generated/urql";
 import { useI18n } from "vue-i18n";
 import { SrcsetEnum } from "dapp-types";
 import { JumboHeader } from "potion-ui";
 import { useRouter, useRoute } from "vue-router";
-import { computed } from "vue";
+import { computed, ref, onMounted, watch } from "vue";
 import { useOnboard } from "@onboard-composable";
+import { useEthersProvider } from "@/composables/useEthersProvider";
+import { usePoolsLiquidity } from "@/composables/useProtocolLiquidity";
+import { useTokenList } from "@/composables/useTokenList";
 
 const { t } = useI18n();
 const router = useRouter();
 const route = useRoute();
 const { connectedWallet } = useOnboard();
+const { blockTimestamp, getBlock } = useEthersProvider();
+const { underlyingsWithLiquidity } = usePoolsLiquidity();
 
 const navigateToPotionCreation = () => router.push("custom-potion-creation");
 
@@ -30,6 +91,17 @@ const jumboIconSrcset = new Map([
   [SrcsetEnum.PNG, "/icons/potion-big.png"],
   [SrcsetEnum.WEBP, "/icons/potion-big.webp"],
 ]);
+
+const underlyingToToken = (address: string, decimals = 18): Token => {
+  const { name, symbol, image } = useTokenList(address);
+  return {
+    address,
+    decimals,
+    name,
+    symbol,
+    image,
+  };
+};
 
 const innerNavProps = computed(() => {
   return {
@@ -54,4 +126,87 @@ const innerNavProps = computed(() => {
     ],
   };
 });
+interface Otoken {
+  id: string;
+  tokenAddress: string;
+  underlyingAsset: {
+    id: string;
+    symbol: string;
+    name: string;
+    address: string;
+    decimals: string;
+  };
+  expiry: string;
+  strikePrice: string;
+}
+
+const loadedMostPurchasedPotions = ref<Otoken[]>([]);
+const loadedMostCollateralizedPotions = ref<Otoken[]>([]);
+
+const mostPurchasedPotionsParams = computed(() => {
+  return {
+    expiry: blockTimestamp.value.toString(),
+    addresses: underlyingsWithLiquidity.value,
+    alreadyLoadedIds:
+      loadedMostPurchasedPotions.value.length > 0
+        ? loadedMostPurchasedPotions.value.map((otoken) => otoken.id)
+        : [""],
+  };
+});
+const mostCollateralizedPotionsParams = computed(() => {
+  return {
+    expiry: blockTimestamp.value.toString(),
+    addresses: underlyingsWithLiquidity.value,
+    alreadyLoadedIds:
+      loadedMostCollateralizedPotions.value.length > 0
+        ? loadedMostCollateralizedPotions.value.map((otoken) => otoken.id)
+        : [""],
+  };
+});
+const { data: mostPurchasedPotions, executeQuery: mostPurchasedPotionsQuery } =
+  useGetMostPurchasedPotionsQuery({
+    variables: mostPurchasedPotionsParams,
+    pause: true,
+  });
+const {
+  data: mostCollateralizedPotions,
+  executeQuery: mostCollateralizedPotionsQuery,
+} = useGetMostCollateralizedPotionsQuery({
+  variables: mostCollateralizedPotionsParams,
+  pause: true,
+});
+
+onMounted(async () => {
+  await getBlock("latest");
+  mostPurchasedPotionsQuery();
+  mostCollateralizedPotionsQuery();
+});
+
+watch(mostPurchasedPotions, () => {
+  if (
+    mostPurchasedPotions.value &&
+    mostPurchasedPotions.value.otokens.length > 0
+  ) {
+    loadedMostPurchasedPotions.value.push(
+      ...mostPurchasedPotions.value.otokens
+    );
+  }
+});
+
+watch(mostCollateralizedPotions, () => {
+  if (
+    mostCollateralizedPotions.value &&
+    mostCollateralizedPotions.value.otokens.length > 0
+  ) {
+    loadedMostCollateralizedPotions.value.push(
+      ...mostCollateralizedPotions.value.otokens
+    );
+  }
+});
+// const handleMostPurchasedLoadMore = () => {
+//   mostPurchasedPotionsQuery();
+// };
+// const handleMostCollateralizedLoadMore = () => {
+//   mostCollateralizedPotionsQuery();
+// };
 </script>
