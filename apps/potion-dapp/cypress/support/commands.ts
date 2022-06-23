a6a8f5a2a0d34360ce66afc9915fe1d8650a50c4
Alberto <albertopastrolin@gmail.com>
2022-06-23 17:38:31 +0200

    chore: use start-local-env to respin tasks on the ci

--- apps/potion-dapp/cypress/support/commands.ts
+++ apps/potion-dapp/cypress/support/commands.ts
@@ -29,7 +29,7 @@
 import "@testing-library/cypress/add-commands";
 
 Cypress.Commands.add("seed", (databasePath) => {
-  cy.exec("cd ../../ && bin/start-local-env-headless", {
+  cy.exec("cd ../../ && bin/start-local-env", {
     env: { DATABASE_PATH: databasePath },
     failOnNonZeroExit: false,
     timeout: 180000,
