49f2850e5a35a5862ff6a36897c22450681840a9
Alberto <albertopastrolin@gmail.com>
2022-06-29 10:22:21 +0200

    feat: improved BaseInput test

--- libs/potion-ui/src/components/BaseInput/BaseInput.cy.ts
+++ libs/potion-ui/src/components/BaseInput/BaseInput.cy.ts
@@ -1,9 +1,28 @@
 /// <reference types="cypress" />
 
-import BaseInput from "./BaseInput.vue";
+import { BaseInput } from "potion-ui";
 
-describe("Test BaseInput. component", () => {
+describe("BaseInput.cy.ts", () => {
   it("is visible", () => {
-    cy.mount(BaseInput).get("input").should("be.visible");
+    cy.mount(BaseInput);
+    cy.get("[test-base-input]").should("be.visible");
+  });
+  it("disables itself if it is not valid", () => {
+    cy.mount(BaseInput, {
+      props: {
+        disabled: true,
+      },
+    });
+    cy.get("[test-base-input]").should("be.visible").and("be.disabled");
+  });
+  it("updates the value when an user type", () => {
+    const onUpdateSpy = cy.spy().as("onUpdateSpy");
+    cy.mount(BaseInput, {
+      props: {
+        "onUpdate:modelValue": onUpdateSpy,
+      },
+    });
+    cy.get("[test-base-input]").clear().type("1001");
+    cy.get("@onUpdateSpy").should("have.been.calledWith", 1001);
   });
 });
