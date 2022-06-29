3f6bdafa7fe4a6bc5d153c21e8128c958cd2d5aa
robercano <3525807+robercano@users.noreply.github.com>
2022-06-29 17:46:01 +0200

    wip (hedging-vault): Fix eslint and typescript config

--- contracts/core/.eslintrc.cjs
+++ contracts/core/.eslintrc.cjs
@@ -1,9 +1,6 @@
 /* eslint-env node */
 module.exports = {
     parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: "./tsconfig.json",
-    },
     extends: ["plugin:import/typescript", "../../.eslintrc.cjs"],
     rules: {
         "@typescript-eslint/no-inferrable-types": "off",
