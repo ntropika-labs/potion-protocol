fdc27546de8a438293e7e9ac1a14baf2315d5419
Raz <me@raz.works>
2022-07-01 09:36:25 +0200

    TokenCard test

--- libs/potion-ui/src/components/TokenCard/TokenCard.cy.ts
+++ libs/potion-ui/src/components/TokenCard/TokenCard.cy.ts
@@ -8,11 +8,19 @@ describe("TokenCard", () => {
     address: "0xMOCKED",
     symbol: "MOCKED",
     image: "https://mocked.com/placeholder.png",
+    onTokenSelected: {},
   };
 
   it("renders properly", () => {
     cy.mount(TokenCard, { props: props });
     cy.get("[test-token-card]");
     cy.get("[test-token-icon]");
+  });
+  it("Emits an event when clicked", () => {
+    const onTokenSelectedSpy = cy.spy().as("onTokenSelectedSpy");
+    props.onTokenSelected = onTokenSelectedSpy;
+    cy.mount(TokenCard, { props: props });
+    cy.get("[test-token-card]").click();
+    cy.get("@onTokenSelectedSpy").should("have.been.calledWith", props.address);
   });
 });
