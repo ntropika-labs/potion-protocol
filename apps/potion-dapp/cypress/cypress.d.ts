21dc1c92a8a6406410700d132420334870d4627d
bained15 <alessio.buscaini92@gmail.com>
2022-07-06 17:46:39 +0200

    fix(potion-dapp): fix cypress typing issues and testing scripts
    - Fix cypress issues with typing the seed custom command
    - Update Nx scripts for e2e-testing to also copy default ganache seeds
    - Remove chrome as the default browser for cypress and use Electron instead

--- apps/potion-dapp/cypress/cypress.d.ts
+++ apps/potion-dapp/cypress/cypress.d.ts
@@ -8,6 +8,14 @@ declare global {
   namespace Cypress {
     interface Chainable {
       mount: typeof mount;
+
+      /**
+       * Custom command to seed ganache with a specific database
+       * @example cy.seed('/a/database/path')
+       * Can also be called specifing a chainTime to start the blockchain at a specific date
+       * @example cy.seed('/a/database/path', '2021-01-01 08:00:00+00:00')
+       */
+      seed(databasePath: string, chainTime?: string): Chainable<Element>;
     }
   }
 }
