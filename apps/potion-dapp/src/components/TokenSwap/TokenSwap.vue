9329d460b9391b35f770c0e5664229d683c5d281
bained15 <alessio.buscaini92@gmail.com>
2022-07-25 15:42:57 +0200

    wip(potion-dapp): vault operator page

---
+++ apps/potion-dapp/src/components/TokenSwap/TokenSwap.vue
@@ -0,0 +1,114 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "TokenSelection",
+});
+</script>
+
+<script lang="ts" setup>
+//import { computed, ref, watch } from "vue";
+import { Token } from "@uniswap/sdk-core";
+// import { ChainId, type SwapRoute } from "@uniswap/smart-order-router";
+
+// import { useAlphaRouter } from "@/composables/useAlphaRouter";
+// import { BaseButton } from "potion-ui";
+
+export interface Props {
+  tokenInput: Token;
+  tokenOutput: Token;
+  inputAmountToSwap: number;
+  recipientAddress: string;
+  routeData: any;
+  routerLoading: boolean;
+}
+
+const props = defineProps<Props>();
+
+// const routePollingEnabled = ref(
+//   Number.isSafeInteger(props.pollingIntervalSecs)
+// );
+// let pollingIntervalId: any = null;
+
+// watch(routePollingEnabled, (doPolling) => {
+//   console.log("toggling polling", doPolling);
+//   if (doPolling === true) {
+//     pollingIntervalId = setInterval(fetchRouteForTokens, 60000);
+//     console.log(
+//       `next polling in ${props.pollingIntervalSecs} seconds`,
+//       pollingIntervalId
+//     );
+//   } else if (pollingIntervalId) {
+//     clearInterval(pollingIntervalId);
+//   }
+// });
+const emit = defineEmits<{
+  (e: "swap-available"): void;
+  (e: "swap-selected", selectedSwap: any): void;
+}>();
+
+console.log(props, emit);
+</script>
+
+<template>
+  <div test-token-swap-wrap class="grid md:grid-cols-3">
+    <div class="md:col-span-2">
+      <p v-if="routerLoading">Loading route...</p>
+      <div v-else-if="routeData">
+        <div>
+          <h2>Routes</h2>
+          <div v-for="(uniRoute, index) in routeData.route" :key="index">
+            <p>Percent: {{ uniRoute.percent }}</p>
+            <p>Pool addresses: {{ uniRoute.poolAddresses }}</p>
+
+            <p>Amount:</p>
+            <pre
+              class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+              >{{ JSON.stringify(uniRoute.amount, null, 2) }}</pre
+            >
+            <p>Quote adjusted for gas:</p>
+            <pre
+              class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+              >{{ JSON.stringify(uniRoute.quoteAdjustedForGas, null, 2) }}</pre
+            >
+            <p>Quote:</p>
+            <pre
+              class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+              >{{ JSON.stringify(uniRoute.quote, null, 2) }}</pre
+            >
+            <p>Route:</p>
+            <pre
+              class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+              >{{ JSON.stringify(uniRoute.route, null, 2) }}</pre
+            >
+            <p>token path:</p>
+            <pre
+              class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+              >{{ JSON.stringify(uniRoute.tokenPath, null, 2) }}</pre
+            >
+            <div class="flex justify-between">
+              <div v-for="(step, index) in uniRoute.tokenPath" :key="index">
+                <pre
+                  class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+                  >{{ JSON.stringify(step, null, 2) }}</pre
+                >
+              </div>
+              <div v-if="index < uniRoute.tokenPath.length - 1">-></div>
+            </div>
+            <!-- <pre
+              class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+              >{{ JSON.stringify(uniswapRouteData.route, null, 2) }}</pre
+            > -->
+          </div>
+        </div>
+        <!-- <div>
+          <p>Trade</p>
+          <pre
+            class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+            >{{ JSON.stringify(uniswapRouteData.trade, null, 2) }}</pre
+          >
+        </div> -->
+      </div>
+      <div v-else>No quote available</div>
+    </div>
+  </div>
+</template>
---
+++ apps/potion-dapp/src/composables/useAlphaRouter.ts
@@ -0,0 +1,98 @@
+import { ref } from "vue";
+import { JsonRpcProvider } from "@ethersproject/providers";
+
+import {
+  AlphaRouter,
+  ChainId,
+  CurrencyAmount,
+  type SwapRoute,
+} from "@uniswap/smart-order-router";
+import { TradeType, Percent, Token } from "@uniswap/sdk-core";
+import JSBI from "jsbi";
+
+import { uniswapRouterUrl } from "@/helpers";
+
+export const useAlphaRouter = (chainId: ChainId) => {
+  const initAlphaRouter = () => {
+    const web3Provider = new JsonRpcProvider(uniswapRouterUrl);
+    return new AlphaRouter({
+      chainId: chainId,
+      provider: web3Provider,
+    });
+  };
+
+  const routerData = ref<SwapRoute | null>(null);
+  const routerPolling = ref(false);
+  const routerLoading = ref(false);
+  const routerError = ref<string | null>(null);
+
+  // Toggle polling for routes
+  const togglePolling = (newState?: boolean) => {
+    if (newState !== undefined) {
+      routerPolling.value = newState;
+    } else {
+      routerPolling.value = !routerPolling.value;
+    }
+  };
+
+  // Get a swap route from input Token to output Token for the input token amount
+  const getRoute = async (
+    inputToken: Token,
+    inputTokenAmount: number,
+    outputToken: Token,
+    recipientAddress: string,
+    slippageToleranceInteger = 1,
+    deadlineTimestamp?: number
+  ) => {
+    routerError.value = null;
+    routerLoading.value = true;
+
+    try {
+      const alphaRouter = initAlphaRouter();
+      const deadline =
+        deadlineTimestamp !== undefined
+          ? deadlineTimestamp
+          : Math.floor(Date.now() / 1000 + 1800);
+      const tokenAmount = CurrencyAmount.fromRawAmount(
+        inputToken,
+        JSBI.BigInt(inputTokenAmount)
+      );
+      const route = await alphaRouter.route(
+        tokenAmount,
+        outputToken,
+        TradeType.EXACT_INPUT,
+        {
+          recipient: recipientAddress,
+          slippageTolerance: new Percent(slippageToleranceInteger, 100),
+          deadline: deadline,
+        }
+      );
+      console.log(`Quote Exact In: ${route?.quote.toFixed(2)}`);
+      console.log(
+        `Gas Adjusted Quote In: ${route?.quoteGasAdjusted.toFixed(2)}`
+      );
+      console.log(`Gas Used USD: ${route?.estimatedGasUsedUSD.toFixed(6)}`);
+
+      // TODO: assert here theres only 1 route returned (no split routes)
+
+      routerData.value = route;
+    } catch (error) {
+      routerError.value =
+        error instanceof Error
+          ? `Cannot get uniswap route: ${error.message}`
+          : "Cannot get uniswap route";
+
+      console.log(error);
+    } finally {
+      routerLoading.value = false;
+    }
+  };
+
+  return {
+    routerData,
+    routerPolling,
+    routerLoading,
+    togglePolling,
+    getRoute,
+  };
+};
