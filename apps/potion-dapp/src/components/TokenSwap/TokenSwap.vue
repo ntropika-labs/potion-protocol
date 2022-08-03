1ceff1631ea3ba88520188584e84148c5398fd8f
bained15 <alessio.buscaini92@gmail.com>
2022-08-03 16:41:18 +0200

    feat(potion-dapp): update TokenSwap component to better display info

--- apps/potion-dapp/src/components/TokenSwap/TokenSwap.vue
+++ apps/potion-dapp/src/components/TokenSwap/TokenSwap.vue
@@ -1,65 +1,106 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useI18n } from "vue-i18n";
+import type { Token } from "dapp-types";
 export default defineComponent({
   name: "TokenSelection",
 });
 </script>
 
 <script lang="ts" setup>
-//import { computed, ref, watch } from "vue";
-import { Token } from "@uniswap/sdk-core";
+import { computed } from "vue";
 import type { SwapRoute } from "@uniswap/smart-order-router";
-// import { ChainId, type SwapRoute } from "@uniswap/smart-order-router";
 
-// import { useAlphaRouter } from "@/composables/useAlphaRouter";
-// import { BaseButton } from "potion-ui";
+import { AssetTag } from "potion-ui";
 
 export interface Props {
-  tokenInput: Token;
-  tokenOutput: Token;
-  inputAmountToSwap: number;
-  recipientAddress: string;
   routeData: SwapRoute | null;
   routerLoading: boolean;
 }
 
 const props = defineProps<Props>();
+const { t } = useI18n();
 
-// const routePollingEnabled = ref(
-//   Number.isSafeInteger(props.pollingIntervalSecs)
-// );
-// let pollingIntervalId: any = null;
-
-// 
-const emit = defineEmits<{
-  (e: "swap-available"): void;
-  (e: "swap-selected", selectedSwap: any): void;
-}>();
-
-console.log(emit);
+const hasRoute = computed(() => props.routeData && props.routeData.route);
+const tradeCurrency = computed(() => props.routeData?.trade.inputAmount);
+const tradeCurrencyToken = computed(
+  () => props.routeData?.trade.inputAmount.currency as Token
+);
+const quoteCurrency = computed(() => props.routeData?.trade.outputAmount);
+const quoteCurrencyToken = computed(
+  () => props.routeData?.trade.outputAmount.currency as Token
+);
+const routes = computed(() => props.routeData?.route);
 </script>
 
 <template>
   <div test-token-swap-wrap>
     <p v-if="routerLoading">Loading route...</p>
-    <div v-else-if="props.routeData">
+    <div v-else-if="hasRoute">
       <div>
-        <h2>Routes</h2>
-        <div v-for="(uniRoute, index) in props.routeData.route" :key="index">
+        <div v-for="(uniRoute, index) in routes" :key="index">
           <p>Percent: {{ uniRoute.percent }}</p>
-          <p>Pool addresses: {{ uniRoute.poolAddresses }}</p>
-
-          <p>Amount:</p>
+          <p>Pool addresses:</p>
           <pre
             class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
-            >{{ uniRoute.amount.toFixed(6) }}</pre
-          >
-          <pre
-            class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
-            >{{ JSON.stringify(uniRoute.amount.currency, null, 2) }}</pre
-          >
-
-          <p>Token path:</p>
+          ><template v-for="(address) in uniRoute.poolAddresses"
+          >{{ address }}</template
+          ></pre>
+          <h3 class="text-xl font-bold">Quote</h3>
+          <hr class="opacity-40 my-4" />
+          <div class="grid md:grid-cols-3 gap-4">
+            <div class="md:col-span-2">
+              <div class="flex flex-row items-start justify-between my-2">
+                <AssetTag
+                  size="xl"
+                  :title="t('currency')"
+                  :token="tradeCurrencyToken"
+                />
+                <div>
+                  <p>Final Quote</p>
+                  <pre
+                    class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+                    >{{ tradeCurrency?.toExact() }}</pre
+                  >
+                  <p>Quote gas adjusted</p>
+                  <pre
+                    class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+                    >{{ props.routeData?.quoteGasAdjusted.toExact() }}</pre
+                  >
+                </div>
+              </div>
+              <div class="text-center">
+                <i class="i-ph-arrow-down-bold w-8 h-8"></i>
+              </div>
+              <div class="flex flex-row items-start justify-between my-2">
+                <AssetTag
+                  size="xl"
+                  :title="t('currency')"
+                  :token="quoteCurrencyToken"
+                />
+                <pre
+                  class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+                  >{{ quoteCurrency?.toExact() }}</pre
+                >
+              </div>
+            </div>
+            <div
+              class="pl-4 border-t-1 md:(border-t-0 border-l-1) border-white border-opacity-20"
+            >
+              <p>Estimated gas used</p>
+              <pre
+                class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+                >{{ props.routeData?.estimatedGasUsed.toNumber() }}</pre
+              >
+              <p>Gas used USD</p>
+              <pre
+                class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+                >{{ props.routeData?.estimatedGasUsedUSD.toFixed(6) }}</pre
+              >
+            </div>
+          </div>
+          <hr class="opacity-40 my-4" />
+          <h3 class="text-xl font-bold">Token path</h3>
           <div class="flex items-center justify-between">
             <div
               v-for="(step, tokenIndex) in uniRoute.tokenPath"
@@ -78,22 +119,30 @@ console.log(emit);
               </div>
             </div>
           </div>
-          <p>Route:</p>
+          <hr class="opacity-40 my-4" />
+          <h3 class="text-xl font-bold">Route</h3>
           <pre
             class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
             >{{ JSON.stringify(uniRoute.route, null, 2) }}</pre
           >
-          <p>token path:</p>
+          <!-- <p>token path:</p>
           <pre
             class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
             >{{ JSON.stringify(uniRoute.tokenPath, null, 2) }}</pre
           >
 
-          <!-- <pre
+          <pre
               class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
               >{{ JSON.stringify(uniswapRouteData.route, null, 2) }}</pre
             > -->
         </div>
+      </div>
+      <div>
+        <p>Parameters</p>
+        <pre
+          class="bg-dark broder-1 border-white rounded-lg m-2 p-4 break-all whitespace-pre-wrap"
+          >{{ JSON.stringify(props.routeData?.methodParameters, null, 2) }}</pre
+        >
       </div>
       <!-- <div>
           <p>Trade</p>
