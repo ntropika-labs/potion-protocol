047bd1f2dd4a19554ad9f7987d406a579216f4d7
Alberto <albertopastrolin@gmail.com>
2022-09-06 17:22:50 +0200

    chore: types and guardings refactor

--- apps/potion-dapp/src/composables/useVaultOperatorCalculations.ts
+++ apps/potion-dapp/src/composables/useVaultOperatorCalculations.ts
@@ -1,53 +1,59 @@
-import { computed, ref, watch, type Ref } from "vue";
+import { computed, ref, watch, unref, isRef } from "vue";
+import { BigNumber } from "ethers";
 import { parseUnits } from "@ethersproject/units";
 
 import { useOracleContract } from "./useOracleContract";
-import { BigNumber } from "ethers";
+
+import type { MaybeRef } from "@vueuse/core";
 
 export function useVaultOperatorCalculations(
-  assetAddress: Ref<string | null>,
-  strikePercentage: Ref<number | null>,
-  principalPercentage: Ref<number | null>,
-  totalAssets: Ref<number>
+  assetAddress: MaybeRef<string | null>,
+  strikePercentage: MaybeRef<number | null>,
+  principalPercentage: MaybeRef<number | null>,
+  totalAssets: MaybeRef<number>
 ) {
-  const pollingTimer = ref<any>(null);
+  const pollingTimer = ref<NodeJS.Timer | null>(null);
   const oraclePrice = ref(0);
   const { getPrice } = useOracleContract();
 
   const strikePrice = computed(() => {
-    if (strikePercentage.value === null) return 0;
-
-    return oraclePrice.value * (strikePercentage.value / 100);
+    const sp = unref(strikePercentage);
+    return sp === null ? 0 : oraclePrice.value * (sp / 100);
   });
 
   const orderSize = computed(() => {
-    if (principalPercentage.value === null) return 0;
-
-    return (
-      (principalPercentage.value / 100) * totalAssets.value * strikePrice.value
-    );
+    const pp = unref(principalPercentage);
+    return pp === null
+      ? 0
+      : (pp / 100) * unref(totalAssets) * strikePrice.value;
   });
 
   const fetchPrice = async (address: string | null) => {
-    if (!address) return;
-
-    const price = await getPrice(address);
-
-    oraclePrice.value = parseFloat(price);
+    if (address !== null) {
+      const price = await getPrice(address);
+      oraclePrice.value = parseFloat(price);
+    }
   };
 
   const numberOfOtokensToBuyBN = computed(() => {
-    if (principalPercentage.value === null) return BigNumber.from(0);
-
-    const ppBN = parseUnits(principalPercentage.value.toString(), 18);
-    const taBN = parseUnits(totalAssets.value.toString(), 18);
-    const dBN = parseUnits("100", 18);
-    const noBN = ppBN.mul(taBN).div(dBN);
-    // otokens are in 8 digits, we need to remove some digits here by dividing by 10^10
-    return noBN.div(10 ** 10);
+    const pp = unref(principalPercentage);
+    const ta = unref(totalAssets);
+    if (pp !== null && ta > 0) {
+      const ppBN = parseUnits(pp.toString(), 18);
+      const taBN = parseUnits(ta.toString(), 18);
+      const dBN = parseUnits("100", 18);
+      // otokens are in 8 digits, we need to remove some digits here by dividing by 10^10
+      return ppBN
+        .mul(taBN)
+        .div(dBN)
+        .div(10 ** 10);
+    }
+    return BigNumber.from(0);
   });
 
-  watch(assetAddress, async (address) => await fetchPrice(address));
+  if (isRef(assetAddress)) {
+    watch(assetAddress, async (address) => await fetchPrice(address));
+  }
 
   const togglePricePolling = (state?: boolean) => {
     const isPolling = Boolean(pollingTimer.value);
@@ -59,10 +65,10 @@ export function useVaultOperatorCalculations(
 
     if (enablePolling) {
       pollingTimer.value = setInterval(
-        () => fetchPrice(assetAddress.value),
+        () => fetchPrice(unref(assetAddress)),
         15000
       );
-    } else {
+    } else if (pollingTimer.value) {
       clearInterval(pollingTimer.value);
       pollingTimer.value = null;
     }
