468e7431eef968e7f92aa50b1cf57aac213a9c1c
bained15 <alessio.buscaini92@gmail.com>
2022-09-08 16:53:56 +0200

    feat(potion-dapp): add utilities to serialize web worker output

--- apps/potion-dapp/src/helpers/vaultOperatorTokens.ts
+++ apps/potion-dapp/src/helpers/vaultOperatorTokens.ts
@@ -1,13 +1,55 @@
-import { Token as UniswapToken, Price } from "@uniswap/sdk-core";
+import { Token as UniswapToken } from "@uniswap/sdk-core";
 
 import { contractsAddresses } from "@/helpers/contracts";
 import { getChainId, getWETHAddress } from "@/helpers/uniswap";
 
 import type { Ref } from "vue";
 import type { Token } from "dapp-types";
-import type { Currency } from "@uniswap/sdk-core";
+
+import type {
+  SwapRoute,
+  V3RouteWithValidQuote,
+} from "@uniswap/smart-order-router";
+import type { UniswapRouterReturn } from "@/types";
+import type { BigNumberish } from "ethers";
 
 const IS_DEV_ENV = import.meta.env.DEV;
+
+const convertUniswapRouteToFlatRoute = (
+  uniRoute: SwapRoute,
+  actionType: "enter" | "exit"
+) => {
+  const routes = uniRoute.route as V3RouteWithValidQuote[];
+
+  const tradeExecutionPrice =
+    actionType === "enter"
+      ? uniRoute.trade.executionPrice.toSignificant(18)
+      : uniRoute.trade.executionPrice.invert().toSignificant(18);
+
+  const uniswapRouterResult: UniswapRouterReturn = {
+    trade: JSON.parse(JSON.stringify(uniRoute.trade)),
+    routes: routes.map((route: V3RouteWithValidQuote) => {
+      return {
+        protocol: route.protocol,
+        inputToken: route.quoteToken as Token,
+        outputToken: route.amount.currency as Token,
+        inputAmount: route.quote.toExact(),
+        outputAmount: route.amount.toExact(),
+        quoteGasAdjusted: route.quoteAdjustedForGas.toExact(),
+        poolAddresses: route.poolAddresses,
+        pools: route.route.pools,
+        percent: route.percent,
+        tokensPath: route.tokenPath.map((t) => t as Token),
+      };
+    }),
+    tradeExecutionPrice: tradeExecutionPrice,
+    methodParameters: uniRoute.methodParameters,
+    estimatedGasUsed: uniRoute.estimatedGasUsed.toString(),
+    estimatedGasUsedUSD: uniRoute.estimatedGasUsedUSD.toExact(),
+  };
+
+  return uniswapRouterResult;
+};
 
 const convertCollateralToUniswapToken = (token: Token): UniswapToken => {
   return new UniswapToken(
@@ -30,25 +72,30 @@ const convertInputToToken = (uniToken: UniswapToken): Token => {
 
 const getEnterExpectedPriceRate = (
   oraclePrice: Ref<number>,
-  tradePrice: Price<Currency, Currency>
+  tradePrice: BigNumberish
 ) => {
   return IS_DEV_ENV
     ? oraclePrice.value //1000,
-    : tradePrice.toSignificant(18); //The expected price of the swap as a fixed point SD59x18 number
+    : tradePrice.toString(); //The expected price of the swap as a fixed point SD59x18 number
 };
 
 const getExitExpectedPriceRate = (
   oraclePrice: Ref<number>,
-  tradePrice: Price<Currency, Currency>
+  tradePrice: BigNumberish
 ) => {
   return IS_DEV_ENV
     ? 1 / oraclePrice.value //0.001,
-    : tradePrice.invert().toSignificant(18); //The expected price of the swap as a fixed point SD59x18 number
+    : tradePrice.toString(); //The expected price of the swap as a fixed point SD59x18 number
 };
 
+const evaluatePremium = (routerPremium: number, premiumSlippage: number) =>
+  routerPremium + (premiumSlippage * routerPremium) / 100;
+
 export {
+  convertUniswapRouteToFlatRoute,
   convertCollateralToUniswapToken,
   convertInputToToken,
   getEnterExpectedPriceRate,
   getExitExpectedPriceRate,
+  evaluatePremium,
 };
