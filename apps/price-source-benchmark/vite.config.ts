a28cbe452b39a35b032771da67e0cfaa29b849aa
bained15 <alessio.buscaini92@gmail.com>
2022-09-26 17:36:59 +0200

    fix(electron-wrapper): add env variable to to sey config options required for electron
    - Assets and files should resolve by a relative path
    - File system should be in strict mode
    - Set the output dir for build to electron-wrapper/render

--- apps/price-source-benchmark/vite.config.ts
+++ apps/price-source-benchmark/vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import path, { join } from "path";
 import Unocss from "unocss/vite";
 import { fileURLToPath, URL } from "url";
 import { defineConfig, searchForWorkspaceRoot } from "vite";
@@ -6,8 +6,13 @@ import { defineConfig, searchForWorkspaceRoot } from "vite";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
 import vue from "@vitejs/plugin-vue";
 
+const VITE_RUNTIME = process.env.VITE_RUNTIME;
+const electron = VITE_RUNTIME === "electron";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: electron ? "" : undefined, // empty path is equal to relative, otherwise set it to undefined to keep the default
+  root: electron ? __dirname : undefined, // dirname when electron, otherwise set it to undefined to keep the default
   plugins: [
     vue({ reactivityTransform: true }),
     vueI18n({
@@ -21,9 +26,22 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    outDir: electron
+      ? path.resolve(__dirname, "../electron-wrapper/render/dist")
+      : undefined, // path to electron-wrapper project when using electron, otherwise set it to undefined to keep the default
+    emptyOutDir: electron ? true : undefined, // empty dist folder when using electron, otherwise set it to undefined to keep the default
+    assetsDir: electron ? "." : undefined, // relative path when electron, otherwise set i t to undefined to keep the default
+    rollupOptions: {
+      input: electron ? join(__dirname, "index.html") : undefined,
+    },
+  },
   server: {
     fs: {
-      allow: [searchForWorkspaceRoot(process.cwd()), "../../libs"],
+      strict: electron ? true : undefined,
+      allow: electron
+        ? undefined
+        : [searchForWorkspaceRoot(process.cwd()), "../../libs"],
     },
   },
 });
