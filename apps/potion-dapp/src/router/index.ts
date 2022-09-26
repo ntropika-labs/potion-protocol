83506061b3bfc7a24d509edbf73094e421ee335e
bained15 <alessio.buscaini92@gmail.com>
2022-09-26 17:32:56 +0200

    fix(electron-wrapper): electron requires web hash navigation

--- apps/potion-dapp/src/router/index.ts
+++ apps/potion-dapp/src/router/index.ts
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 import { useOnboard } from "@onboard-composable";
 
 import BaseLayout from "@/layouts/BaseLayout.vue";
@@ -16,15 +20,13 @@ const PoolTemplate = () => import("@/views/PoolTemplate.vue");
 const ShowPotion = () => import("@/views/Potions/ShowPotion.vue");
 const ViewPotions = () => import("@/views/Potions/ViewPotions.vue");
 
+console.log("VITE_RUNTIME:", import.meta.env.VITE_RUNTIME);
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history:
+    import.meta.env.VITE_RUNTIME === "electron"
+      ? createWebHashHistory()
+      : createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/:pathMatch(.*)*",
-      name: "NotFound",
-      component: NotFound,
-      meta: { layout: EmptyLayout },
-    },
     {
       path: "/",
       name: "home",
@@ -100,6 +102,12 @@ const router = createRouter({
       name: "show-potion",
       component: ShowPotion,
       meta: { requiredWallet: false, layout: BaseLayout },
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: NotFound,
+      meta: { layout: EmptyLayout },
     },
   ],
 });
