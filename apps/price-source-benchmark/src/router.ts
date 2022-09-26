7660575d9e03dffa5b92d19b5474e5adba813ddc
bained15 <alessio.buscaini92@gmail.com>
2022-09-26 17:33:58 +0200

    fix(electron-wrapper): electron requires web hash navigation

--- apps/price-source-benchmark/src/router.ts
+++ apps/price-source-benchmark/src/router.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 import BenchmarkPage from "./views/BenchmarkPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history:
+    import.meta.env.VITE_RUNTIME === "electron"
+      ? createWebHashHistory()
+      : createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
