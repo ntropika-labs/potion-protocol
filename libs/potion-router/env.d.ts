3aaa4c96a9881e06206dde93eff42e180f2e6a2d
Alberto <albertopastrolin@gmail.com>
2022-09-27 16:26:37 +0200

    feat: build libs with tsc

--- libs/potion-router/env.d.ts
+++ libs/potion-router/env.d.ts
@@ -1,11 +1,6 @@
+/// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_ALCHEMY_KEY: string;
-  readonly VITE_ETHEREUM_NETWORK: "localhost" | "ganache" | "kovan" | "mainnet";
   readonly VITE_SUBGRAPH_ADDRESS: string;
-  readonly VITE_ENDPOINT_PROVIDER: string;
-  readonly VITE_INFURA_KEY: string;
-  readonly VITE_DEVELOPMENT_MNEMONIC: string;
-  readonly VITE_DEVELOPMENT_ADDRESS: string;
 }
 
 interface ImportMeta {
