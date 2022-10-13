eafb9cdc5b006b3949fc06f76f75662b780cb6d8
Alberto <albertopastrolin@gmail.com>
2022-10-13 16:02:12 +0200

    feat(CU-3jn9pez): load multivault addresses + helpers

--- apps/potion-dapp/src/helpers/hedgingVaultContracts.ts
+++ apps/potion-dapp/src/helpers/hedgingVaultContracts.ts
@@ -1,5 +1,21 @@
 import { Deployments } from "@potion-protocol/hedging-vault";
+import multivault from "@potion-protocol/hedging-vault/multivault.json";
 import { ethereumNetwork } from "./";
 
 //@ts-expect-error iterator is not defined
 export const contractsAddresses = Deployments[ethereumNetwork].contracts;
+
+export const multiVaultAddresses = [
+  {
+    vault: contractsAddresses.InvestmentVault.address.toLowerCase(),
+    potionBuyAction: contractsAddresses.PotionBuyAction.address.toLowerCase(),
+  },
+].concat(multivault);
+
+export function getPotionBuyActionFromVault(vault: string) {
+  const data = multiVaultAddresses.find((mv) => mv.vault === vault);
+  if (data) {
+    return data.potionBuyAction;
+  }
+  throw `${vault} isn't a recognized vault`;
+}
