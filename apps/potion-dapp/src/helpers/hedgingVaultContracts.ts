fb689562427b35814e348235115ae0a083261244
Alberto <albertopastrolin@gmail.com>
2022-11-02 11:23:35 +0100

    feat: generate multivault file with a script and use it in the dapp

--- apps/potion-dapp/src/helpers/hedgingVaultContracts.ts
+++ apps/potion-dapp/src/helpers/hedgingVaultContracts.ts
@@ -1,22 +1,16 @@
 import { Deployments } from "@potion-protocol/hedging-vault";
-import multivault from "@potion-protocol/hedging-vault/multivault.json";
+import multivault from "../../multivault.json";
 import { ethereumNetwork } from "./";
 
 //@ts-expect-error iterator is not defined
 export const contractsAddresses = Deployments[ethereumNetwork].contracts;
 
-// TODO: we will need to change the format to include also the contracts for deferred deposit and withdraw
-export const multiVaultAddresses = [
-  {
-    vault: contractsAddresses.InvestmentVault.address.toLowerCase(),
-    potionBuyAction: contractsAddresses.PotionBuyAction.address.toLowerCase(),
-  },
-].concat(multivault);
-
 export function getPotionBuyActionFromVault(vault: string) {
-  const data = multiVaultAddresses.find((mv) => mv.vault === vault);
+  const data = multivault.find(
+    (mv) => mv.InvestmentVault.toLowerCase() === vault.toLowerCase()
+  );
   if (data) {
-    return data.potionBuyAction;
+    return data.PotionBuyAction;
   }
   throw `${vault} isn't a recognized vault`;
 }
