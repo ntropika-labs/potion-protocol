97de9f58eeb3141cfca93121de553943b30606cb
Alessio <97225583+bained15@users.noreply.github.com>
2022-11-21 10:00:09 +0100

    Feat discover hedging vaults page cu 33tekdw (#161)
    * feat(subgraph-hv): update hedging vaults query
    
    * feat(potion-dapp): update hedging vaults  types
    
    * feat(potion-dapp): develop helper to group hedging vaults for discovery page
    
    * feat(potion-dapp): updatecomponents for hv discovery page
    
    * feat(potion-dapp): update hv discovery page

--- apps/potion-dapp/src/composables/useHedgingVaults.ts
+++ apps/potion-dapp/src/composables/useHedgingVaults.ts
@@ -1,18 +1,9 @@
 import { useGetHedgingVaultsQuery } from "subgraph-queries-hv/generated/urql";
 import type { GetHedgingVaultsQuery } from "subgraph-queries-hv/generated/operations";
-import { computed } from "vue";
+import { computed, ref, type Ref } from "vue";
 
-import type { Token } from "dapp-types";
 import { formatUnits } from "@ethersproject/units";
-
-interface VaultData {
-  address: string;
-  asset: Token;
-  shareToken: Token;
-  size: string;
-  hedgingRate: string;
-  strikePercentage: string;
-}
+import type { RoundsFragment, VaultData } from "@/types";
 
 type HedgingVaultFragment = GetHedgingVaultsQuery["hedgingVaults"][0];
 
@@ -30,26 +21,45 @@ const fragmentToData = (fragment: HedgingVaultFragment): VaultData => ({
     symbol: fragment.shareToken.symbol,
     decimals: parseInt(fragment.shareToken.decimals),
   },
+  action: {
+    maxPremium: formatUnits(fragment?.action?.maxPremiumPercentage ?? "0", 6),
+    cycleDurationInSecs: formatUnits(
+      fragment.action?.cycleDurationSecs ?? "0",
+      0
+    ),
+  },
+  rounds: (fragment?.rounds ?? []) as unknown as RoundsFragment[],
   size: fragment.totalAssets,
   hedgingRate: formatUnits(fragment?.action?.hedgingRate ?? "0", 6),
   strikePercentage: formatUnits(fragment.action?.strikePercentage ?? "0", 6),
 });
 
-const useHedgingVaults = () => {
-  const { data, fetching, error } = useGetHedgingVaultsQuery({
+const useHedgingVaults = (investor: Ref<string>) => {
+  const vaults: Ref<Array<VaultData>> = ref([]);
+  const variables = computed(() => ({
+    investor: investor.value,
+  }));
+
+  const { fetching, error, executeQuery } = useGetHedgingVaultsQuery({
+    variables,
     context: {
       url: import.meta.env.VITE_SUBGRAPH_HV_ADDRESS,
     },
   });
 
-  const vaults = computed(
-    () => data.value?.hedgingVaults?.map(fragmentToData) ?? []
-  );
+  async function loadVaults() {
+    const { data } = await executeQuery();
+    const loadedVaults = data.value?.hedgingVaults?.map(fragmentToData) ?? [];
+    vaults.value = loadedVaults;
+
+    return loadedVaults;
+  }
 
   return {
     vaults,
     error,
     loading: fetching,
+    loadVaults,
   };
 };
 
