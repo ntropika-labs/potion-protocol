1d8a1f23df1e04184e7796ff82621178f1a63c9d
Alberto <albertopastrolin@gmail.com>
2022-11-25 11:49:32 +0100

    fix: lowercase addresses in useTokenList

--- apps/potion-dapp/src/composables/useTokenList.ts
+++ apps/potion-dapp/src/composables/useTokenList.ts
@@ -4,7 +4,9 @@ console.info(import.meta.env.VITE_ETHEREUM_NETWORK);
 const tokenList = getTokenList(import.meta.env.VITE_ETHEREUM_NETWORK);
 
 const useTokenList = (address: string) => {
-  const token = tokenList.find((token) => token.address === address);
+  const token = tokenList.find(
+    (token) => token.address.toLowerCase() === address.toLowerCase()
+  );
 
   const name = token?.name ?? "";
   const symbol = token?.symbol ?? "";
