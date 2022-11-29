2bbad61f4512ce8838bd7354496a123863f8403a
bained15 <alessio.buscaini92@gmail.com>
2022-11-29 15:49:49 +0100

    fix(potion-dapp): fix VaultCard navigation to etherscan link

--- libs/potion-ui/src/components/VaultCard/VaultCard.vue
+++ libs/potion-ui/src/components/VaultCard/VaultCard.vue
@@ -5,6 +5,7 @@ import { getEtherscanUrl } from "../../helpers";
 
 import TokenIcon from "../TokenIcon/TokenIcon.vue";
 import BaseCard from "../BaseCard/BaseCard.vue";
+import BaseButton from "../BaseButton/BaseButton.vue";
 
 import LabelValue from "../LabelValue/LabelValue.vue";
 import BaseTag from "../BaseTag/BaseTag.vue";
@@ -24,10 +25,6 @@ const props = withDefaults(
   { currency: "USDC", strategy: VaultStrategy.PROTECTIVE_PUT }
 );
 
-const emits = defineEmits<{
-  (e: "selected"): void;
-}>();
-
 const { t } = useI18n();
 
 const roundLength = computed(
@@ -45,7 +42,13 @@ const strategyName = computed(() => {
       return t("protective_put");
   }
 });
-const vaultEtherscanLink = getEtherscanUrl(props.address);
+const vaultEtherscanLink = computed(() => getEtherscanUrl(props.address));
+const vaultEtherscanLabel = computed(
+  () => props.address.substring(0, 8) + "..."
+);
+const callbackNavigateToEtherscan = (url: string) => {
+  window && window.open(url, "_blank")?.focus();
+};
 </script>
 
 <template>
@@ -55,71 +58,67 @@ const vaultEtherscanLink = getEtherscanUrl(props.address);
     :full-height="false"
     test-vault-card
   >
-    <a href="#" test-button @click.prevent="emits('selected')">
-      <div
-        class="group relative grid grid-flow-row grid-cols-2 justify-between items-center gap-6 p-6 transition-all hover:(bg-gradient-to-br bg-opacity-100 from-primary-500 via-primary-400 to-primary-600)"
-      >
-        <div class="col-span-2 grid grid-cols-2 justify-between gap-6">
-          <BaseTag
-            size="md"
-            class="!capitalize group-hover:bg-black/20 transition-none"
-            test-strategy
-            >{{ strategyName }}</BaseTag
-          >
-          <a
-            :href="vaultEtherscanLink"
-            class="w-full flex items-center justify-center hover:underline"
-          >
-            <BaseTag
-              size="md"
-              class="group-hover:bg-black/20 hover:!bg-black/70 transition-none"
-              test-address
-            >
-              {{ props.address.substring(0, 8) }}...
-              <i class="i-ph-arrow-square-in mr-1"></i>
-            </BaseTag>
-          </a>
-        </div>
-
-        <div class="flex flex-col gap-2">
-          <span>{{ t("covered_asset") }}</span>
-          <TokenIcon
-            class="rounded-full bg-deep-black-700 mr-2"
-            :address="props.asset.address"
-            :name="props.asset.name"
-            :image="props.asset.image"
-            size="2xl"
-            test-asset
-          />
-        </div>
-        <LabelValue
-          size="lg"
-          :title="t('strike')"
-          :value="props.strike.toString()"
-          value-type="number"
-          value-size="3xl"
-          symbol="%"
-          test-strike-percentage
-        ></LabelValue>
-        <LabelValue
-          size="lg"
-          :title="t('max_premium')"
-          :value="props.maxPremium.toString()"
-          value-type="number"
-          value-size="3xl"
-          symbol="%"
-          test-max-premium
-        ></LabelValue>
-        <LabelValue
-          size="lg"
-          :title="t('round_length')"
-          :value="roundLength.toString()"
-          value-type="raw"
-          value-size="3xl"
-          symbol="d"
-          test-round-length
-        ></LabelValue>
+    <div
+      class="group relative grid grid-flow-row grid-cols-2 justify-between items-center gap-6 p-6 transition-all hover:(bg-gradient-to-br bg-opacity-100 from-primary-500 via-primary-400 to-primary-600)"
+    >
+      <div class="col-span-2 grid grid-cols-2 justify-between gap-6">
+        <BaseTag
+          size="md"
+          class="!capitalize group-hover:bg-black/20 transition-none"
+          test-strategy
+          >{{ strategyName }}</BaseTag
+        >
+        <BaseButton
+          :label="vaultEtherscanLabel"
+          size="sm"
+          palette="flat"
+          class="!rounded bg-white/10 !hover:bg-black/40 group-hover:bg-black/20 transition-none z-4"
+          test-address
+          @click.prevent="callbackNavigateToEtherscan(vaultEtherscanLink)"
+        >
+          <template #post-icon>
+            <i class="i-ph-arrow-square-in mr-1"></i> </template
+        ></BaseButton>
       </div>
-    </a>
+
+      <div class="flex flex-col gap-2">
+        <span>{{ t("covered_asset") }}</span>
+        <TokenIcon
+          class="rounded-full bg-deep-black-700 mr-2"
+          :address="props.asset.address"
+          :name="props.asset.name"
+          :image="props.asset.image"
+          size="2xl"
+          test-asset
+        />
+      </div>
+      <LabelValue
+        size="lg"
+        :title="t('strike')"
+        :value="props.strike.toString()"
+        value-type="number"
+        value-size="3xl"
+        symbol="%"
+        test-strike-percentage
+      ></LabelValue>
+      <LabelValue
+        size="lg"
+        :title="t('max_premium')"
+        :value="props.maxPremium.toString()"
+        value-type="number"
+        value-size="3xl"
+        symbol="%"
+        test-max-premium
+      ></LabelValue>
+      <LabelValue
+        size="lg"
+        :title="t('round_length')"
+        :value="roundLength.toString()"
+        value-type="raw"
+        value-size="3xl"
+        symbol="d"
+        test-round-length
+      ></LabelValue>
+    </div>
   </BaseCard>
 </template>
